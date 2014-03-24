d95f71f73554fa77a6af01408576724088a0fd87
Kunal Mehta <legoktm@gmail.com>
2014-03-23 23:48:25 -0700

    Initial commit

---
+++ plugin.py
@@ -0,0 +1,18 @@
+__module_name__ = "rhthymbox"
+__module_version__ = "1.0"
+__module_description__ = "Plugin for Rhythymbox integration"
+
+import hexchat
+import subprocess
+
+
+def np(word, word_eol, userdata):
+    output = subprocess.check_output([
+        'rhythmbox-client',
+        '--print-playing-format',
+        '%tt - %ta'
+    ])
+    output = output.strip()  # Has a trailing newline
+    hexchat.command('me NP: {}'.format(output))
+
+hexchat.hook_command('np', np)
